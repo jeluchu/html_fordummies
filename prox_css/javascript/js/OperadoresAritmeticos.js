8e03ffcd61cd56c8844304926290eaae8b3146db
Jesús Mª <jeluchu@gmail.com>
2018-04-26 10:15:07 +0200

    NEW 05 OPERADORES ARITMETICOS

---
+++ prox_css/javascript/js/OperadoresAritmeticos.js
@@ -0,0 +1,29 @@
+function CalcularOperadores () {
+	
+	/* 
+	   Realiza operaciones aritméticas
+	   con elementos del documento HTML
+	*/
+
+	var miParrafo;
+	var valor;
+
+	miParrafo = document.getElementById('Resultado');
+
+	valor = 12 + 20;
+	miParrafo.innerHTML +=
+		"Sumando los numeros 12 y 20 = " + valor + "<br>";
+
+	valor = 12 - 20;
+	miParrafo.innerHTML +=
+		"Restando los numeros 12 y 20 = " + valor + "<br>";
+
+	valor = 3 * 20;
+	miParrafo.innerHTML +=
+		"Multiplicando los numeros 3 y 20 = " + valor + "<br>";
+
+	valor = 34 / 6;
+	miParrafo.innerHTML +=
+		"Dividiendo los numeros 34 y 6 = " + valor + "<br>";
+
+}
