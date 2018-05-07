433626ec974d11ec5535ca12b0666ff2e93e9d9d
Jesús Mª <jeluchu@gmail.com>
2018-05-07 13:21:39 +0200

    NEW 07 - CONDICION SIMPLE

---
+++ prox_css/javascript/js/CondicionalSimple.js
@@ -0,0 +1,26 @@
+function CalcularEtapa() {
+	
+	var campoEdad,
+		campoResultado,
+		valorEdad;
+
+	campoResultado = document.getElementById('Resultado');
+	campoEdad = document.getElementById('Edad');
+	valorEdad = campoEdad.value;
+	campoResultado.innerHTML = "Tu etapa es: ";
+
+	if (valorEdad >= 23)
+		campoResultado.innerHTML += "Adulto";
+	else
+	if ((valorEdad <= 22) && (valorEdad >= 17))
+		campoResultado.innerHTML += "Juventud";
+	else
+	if ((valorEdad <= 16) && (valorEdad >= 13))
+		campoResultado.innerHTML += "Adolescente";
+	else
+	if ((valorEdad >= 0) && (valorEdad <= 12))
+		campoResultado.innerHTML += "Infancia";
+	else
+	if (valorEdad < 0)
+		campoResultado.innerHTML += "Estás intentando jugar con el programa";
+}
