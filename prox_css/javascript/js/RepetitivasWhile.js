d6d131ee8b7d2bdec33f7cb233fcdfba5b251f05
Jesús Mª <jeluchu@gmail.com>
2018-05-17 09:28:21 +0200

    NEW UPDATE

--- prox_css/javascript/js/RepetitivasWhile.js
+++ prox_css/javascript/js/RepetitivasWhile.js
@@ -1,13 +1,31 @@
 function EscribirNumeros() 
 {
 	var campoResultado,
+		contador,
 		numero;
 
 	campoResultado = document.getElementById('Resultado');
 	numero = parseInt(document.getElementById('Numero').value);
-	contador = 0;
+	contador = 1;
 
 	// Escribir los números del 1 al número:
 	// 		- Con While
+
+	while (contador <= numero)
+	{
+		campoResultado.innerHTML += " " + contador++;
+	}
+
 	//		- Con Do-While
+
+	contador = 1;
+	do 
+	{
+
+		campoResultado.innerHTML += " " + contador++;
+
+	} while (contador <= numero)
+
+
+
 }
